2b5b533fb863ae95430e423634fbfad016d8e1ad
wu_tian_plai <2212929908@qq.com>
2025-03-21 15:59:59 +0800

    修复了文本模态框关闭按钮没有渲染出来的问题

--- static/js/utils/attachments/modal/TextModal.js
+++ static/js/utils/attachments/modal/TextModal.js
@@ -62,7 +62,12 @@ export class TextModal {
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">文本预览</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                            </svg>
+                        </button>
                     </div>
                     <div class="modal-body">
                         <div class="text-info mb-3">
@@ -85,7 +90,12 @@ export class TextModal {
                     </div>
                 </div>
             </div>
-            <button class="modal-mobile-close-btn" data-bs-dismiss="modal" aria-label="关闭">&times;</button>
+            <button class="modal-mobile-close-btn" data-bs-dismiss="modal" aria-label="关闭">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </button>
         `;
 
         this.contentElement = this.modalElement.querySelector('.code-table tbody');
